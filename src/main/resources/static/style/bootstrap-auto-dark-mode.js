b9039f437ef573f7f6b9cec8df2a143707a8141d
KristofferKay <37834268+KristofferKay@users.noreply.github.com>
2024-10-16 22:21:30 +0200

    Added auto dark, light mode

--- src/main/resources/static/style/bootstrap-auto-dark-mode.js
+++ src/main/resources/static/style/bootstrap-auto-dark-mode.js
@@ -3,7 +3,7 @@
     if(htmlElement.getAttribute("data-bs-theme") === 'auto') {
         function updateTheme() {
             document.querySelector("html").setAttribute("data-bs-theme",
-                window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" :  "light")
+                window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
         }
 
         window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme)
