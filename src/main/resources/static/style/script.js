524d6ca9ce8786dc82c90195d8c33b049f33a0f9
KristofferKay <37834268+KristofferKay@users.noreply.github.com>
2024-10-18 16:13:46 +0200

    fixed script.js to set price to 0 when checkbox is active.

--- src/main/resources/static/style/script.js
+++ src/main/resources/static/style/script.js
@@ -1,12 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
     const freeCheckbox = document.getElementById('FREE'); // Make sure this ID matches your tag for 'Free'
     const priceField = document.getElementById('priceField');
+    const setPriceToZero = document.getElementById('price')
+
 
     function togglePriceField() {
-        if (freeCheckbox.checked) {
+        console.log(setPriceToZero)
+
+        if (freeCheckbox && freeCheckbox.checked) {
             priceField.style.display = 'none';
+            setPriceToZero.value = "0.0";
+
         } else {
             priceField.style.display = 'block';
+
         }
     }
 
@@ -15,4 +22,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Add event listener to checkbox
     freeCheckbox.addEventListener('change', togglePriceField);
+
+    if (freeCheckbox) {
+        freeCheckbox.addEventListener('change', togglePriceField);
+    }
 });
